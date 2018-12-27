d2fe19bb15b5e55753443486f51ee87c50308c19
HamzaAlwan <ihamzaalwan@gmail.com>
2018-12-28 00:10:57 +0200

    removed Error Boundary component

--- src/Container/App.js
+++ src/Container/App.js
@@ -3,7 +3,6 @@ import React, { Component } from 'react';
 import './App.css';
 import Cards from '../Components/Cards/Cards';
 import SearchBox from '../Components/SearchBox/SearchBox';
-import ErrorBoundary from '../Components/ErrorBoundary';
 
 class App extends Component {
 	constructor(){
@@ -24,9 +23,7 @@ class App extends Component {
 			<div className="tc">
 				<h1>Robo Users</h1>
 				<SearchBox searchChange={this.onSearchChange} />
-				<ErrorBoundary>
-					<Cards search={this.state.searchField} />
-				</ErrorBoundary>
+				<Cards search={this.state.searchField} />
 			</div>
 		);
 	}
